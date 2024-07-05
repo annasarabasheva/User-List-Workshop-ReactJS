b8a390fc342e2a94f5801b5ce2b6b9c439efb0bb
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-05 13:31:28 +0300

    Add header and footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
  
 
   return (
     <>
-      <h1>Hey there</h1>
+      <Header />
+      <Footer />
     </>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <div>
+            <footer class="footer">
+                <p>This site is designed to be used for training purposes at SoftUni.</p>
+            </footer>
+        </div>
+    )
+
+
+}
+
+
+export default Footer
