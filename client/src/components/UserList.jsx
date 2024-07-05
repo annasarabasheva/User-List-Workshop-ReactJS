c1f354c728edd102a8fc3fa54f8ef7c708bf0e07
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-07-05 14:06:35 +0300

    Add button

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,10 +1,12 @@
 import Search from "./Search"
+import UserListTable from "./UserListTable"
 
 function UserList() {
     return  (
         <section className="card users-container">
-            
             <Search/>
+            <UserListTable/>
+            <button className="btn-add btn">Add new user</button>
         </section>
     )
 }
